af06111e03a7bdcce17d916366171143cd051597
BOPOHOB <bopohob@yandex-team.ru>
2021-06-27 07:50:13 +0300

    wrap kernel to npm package, gui prove of concept

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useMemo, useState } from 'react';
+import type * as Kernel from 'kernel';
 import './App.css';
 
 const Liquids = {
@@ -15,7 +15,10 @@ const Liquids = {
   'brown': '#774b1a',
   'coral': '#db8f51',
   'yellow': '#ecda6c',
+  'empty': 'rgba(0,0,0,0)',
 };
+
+const LiquidsNames = Object.fromEntries(Object.entries(Liquids).map(([a, b]) => [b, a]));
 
 const data = [
     [
@@ -90,21 +93,67 @@ const data = [
         Liquids['green'],
         Liquids['purple'],
     ],
+    [
+        Liquids['empty'],
+        Liquids['empty'],
+        Liquids['empty'],
+        Liquids['empty'],
+    ],
+    [
+        Liquids['empty'],
+        Liquids['empty'],
+        Liquids['empty'],
+        Liquids['empty'],
+    ],
 ];
 
+const Flasks = ({ flasks }: any) => (
+  <>
+    {
+      flasks.map((flask: any, id: number) => (
+        <div key={id}>
+          {
+            flask.map((liquid: any, lId: number) => <div key={lId} style={{backgroundColor: liquid}} />)
+          }
+        </div>
+      ))
+    }
+    <br />
+    <br />
+    <br />
+    <br />
+  </>
+);
+
+const kernelData = data.map((flask) => flask.map((liquid) => {
+  let name = LiquidsNames[liquid];
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}));
+
+const fromKernel = (flasks: string[][]) => flasks.map((flask: string[]) =>
+// @ts-ignore
+  flask.map((liquid: string): any => Liquids[liquid.toLowerCase()])
+);
+
 function App() {
+  let [wasm, takeWasm] = useState(null);
+  useEffect(() => import('kernel').then(takeWasm as any) as any, []);
+  let solution = useMemo(() => {
+    if (!wasm) {
+      return null;
+    }
+    return (wasm as any)?.search(kernelData);
+  }, [wasm, data]);
+  console.log(solution);
   return (
     <div className="App">
-      {
-        data.map((flask) => (
-          <div key={flask.join(',')}>
-            {
-              flask.map((liquid) => <div style={{backgroundColor: liquid}} />)
-            }
-          </div>
-        ))
-      }
-
+      <div><Flasks flasks={data} /></div>
+      { solution && (
+        solution.map((_: any, id: number) => {
+          let flasks = (wasm as any).get_step(kernelData, solution.slice(0, id))
+          return <div key={id}><Flasks flasks={fromKernel(flasks)} /></div>;
+        })
+      ) }
     </div>
   );
 }
